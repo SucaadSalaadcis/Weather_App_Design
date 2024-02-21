0901c1dfb45f177b68f1169636eaee3896ad2e8d
sucaad salaad <SucaadSalaad520@gmail.com>
2024-02-20 18:20:10 -0800

    first commit

---
+++ main.js
@@ -0,0 +1,10 @@
+
+const apikey= '';
+const listCard = document.querySelector(".list-card");
+const list = document.querySelector(".list");
+const searchCity = document.querySelector("#searchCity");
+const cityName = document.querySelector("#searchCity");
+const weatherText = document.querySelector("#weatherText");
+const degree = document.querySelector("#degree");
+const imageTime = document.querySelector("#imageTime");
+
